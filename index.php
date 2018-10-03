644d6fe0564c8d98ac2364d5044feb15cf33765f
Oleg Sher <github@sher.biz>
2018-10-04 00:10:29 +0300

    dfdfh

---
+++ index.php
@@ -0,0 +1,12 @@
+<!doctype html>
+<html lang="en">
+  <head>        
+    <title>Hello, world!</title>
+    <?php include("_header_include.html"); ?>
+  </head>
+  <body>
+    <h2><a href="ru/">Русский</a><br>
+    <a href="en/">English</a></h2>
+	<?php include("_bottom_include.html"); ?>
+   </body>
+</html>
